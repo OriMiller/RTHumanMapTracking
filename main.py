d9be050fe4ca1c4e5f6ab6081279f92c3530142c
omiller <skateboarderom@gmail.com>
2020-06-22 10:00:39 -0600

    reshaping roi using warpPerspective

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-points = []
+inputPoints = []
+outputPoints = []
+ipts = np.array([])
+opts = np.array([])
 bound = False
+state = False
 
 
 def addpoint(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        points.append((x, y))
+        if not state:
+            inputPoints.append((x, y))
+
+        else:
+            outputPoints.append((x, y))
 
 
 cap = cv2.VideoCapture(0)
@@ -25,19 +33,49 @@ while cap.isOpened():
     dim = (width, height)
     resized = cv2.resize(frame, dim)
 
-    npts = np.array(points)
-    polyim = cv2.polylines(resized, [npts], True, (255, 0, 0))
-    cv2.imshow("polyim", polyim)
-    for p in points:
-        cv2.circle(polyim, p, 3, (255, 0, 0))
+    if bound:
+        rows, cols, ch = resized.shape
 
+        pts1 = np.float32(ipts)
+        pts2 = np.float32(opts)
+
+        M = cv2.getPerspectiveTransform(pts1, pts2)
+        dst = cv2.warpPerspective(resized, M, (300, 300))
+
+        cv2.imshow("Output", dst)
+        cv2.imshow("Input", resized)
+
+    else:
+        if not state:
+            cv2.putText(resized, "Click 4 points, Press 'N' when done", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0))
+
+        else:
+            cv2.putText(resized, "Click 4 new points, Press 'S' when Done", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0))
+
+        ipts = np.array(inputPoints)
+        opts = np.array(outputPoints)
+        if not state:
+            polyim = cv2.polylines(resized, [ipts], True, (255, 0, 0))
+            cv2.imshow("polyim", polyim)
+            for p in inputPoints:
+                cv2.circle(polyim, p, 3, (255, 0, 0))
+
+        else:
+            polyim = cv2.polylines(resized, [opts], True, (255, 0, 0))
+            cv2.imshow("polyim", polyim)
+            for p in outputPoints:
+                cv2.circle(polyim, p, 3, (255, 0, 0))
 
     key = cv2.waitKey(1)
     if key == ord('s'):
-        cv2.waitKey()
+        bound = True
+        cv2.destroyWindow("polyim")
+
+    elif key == ord('n'):
+        state = True
 
     elif key == ord('q'):
-        break;
+        break
 
 cap.release()
 cv2.destroyAllWindows()
