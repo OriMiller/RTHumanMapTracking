f26a6104cd98fc357743ffb6869fddb877c6cda8
omiller <skateboarderom@gmail.com>
2020-06-22 17:18:28 -0600

    reshaping roi using warpPerspective

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+outputSize = 500
 inputPoints = []
 outputPoints = []
+blue = (255, 0, 0)
 ipts = np.array([])
-opts = np.array([])
 bound = False
 state = False
 
 
 def addpoint(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        if not state:
-            inputPoints.append((x, y))
-
-        else:
-            outputPoints.append((x, y))
+        inputPoints.append((x, y))
 
 
 cap = cv2.VideoCapture(0)
@@ -31,48 +28,41 @@ while cap.isOpened():
     width = int(frame.shape[1] * scale_percent / 100)
     height = int(frame.shape[0] * scale_percent / 100)
     dim = (width, height)
-    resized = cv2.resize(frame, dim)
+    resizedImg = cv2.resize(frame, dim)
 
     if bound:
-        rows, cols, ch = resized.shape
+        rows, cols, ch = resizedImg.shape
 
         pts1 = np.float32(ipts)
-        pts2 = np.float32(opts)
+        pts2 = np.float32([[0, 0], [0, outputSize], [outputSize, outputSize], [outputSize, 0]])
 
         M = cv2.getPerspectiveTransform(pts1, pts2)
-        dst = cv2.warpPerspective(resized, M, (300, 300))
+        warpedImg = cv2.warpPerspective(resizedImg, M, (outputSize, outputSize))
 
-        cv2.imshow("Output", dst)
-        cv2.imshow("Input", resized)
+        cv2.imshow("Input", resizedImg)
+        cv2.imshow("Output", warpedImg)
 
     else:
-        if not state:
-            cv2.putText(resized, "Click 4 points, Press 'N' when done", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0))
-
-        else:
-            cv2.putText(resized, "Click 4 new points, Press 'S' when Done", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255, 0, 0))
-
+        cv2.putText(resizedImg, "Click 4 points counter clockwise from top left, Press 'S' when Done", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
         ipts = np.array(inputPoints)
         opts = np.array(outputPoints)
         if not state:
-            polyim = cv2.polylines(resized, [ipts], True, (255, 0, 0))
+            polyim = cv2.polylines(resizedImg, [ipts], True, blue)
             cv2.imshow("polyim", polyim)
             for p in inputPoints:
-                cv2.circle(polyim, p, 3, (255, 0, 0))
+                cv2.circle(polyim, p, 3, blue)
 
         else:
-            polyim = cv2.polylines(resized, [opts], True, (255, 0, 0))
+            polyim = cv2.polylines(resizedImg, [opts], True, blue)
             cv2.imshow("polyim", polyim)
             for p in outputPoints:
-                cv2.circle(polyim, p, 3, (255, 0, 0))
+                cv2.circle(polyim, p, 3, blue)
 
     key = cv2.waitKey(1)
     if key == ord('s'):
+        print(inputPoints)
         bound = True
         cv2.destroyWindow("polyim")
-
-    elif key == ord('n'):
-        state = True
 
     elif key == ord('q'):
         break
